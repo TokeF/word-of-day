ec72ff60ca9062f5eb0654311ad12533289b9e48
TokeF <Toke1993@hotmail.com>
2025-03-04 14:08:32 +1100

    remove global styles

--- app/(screens)/categories.tsx
+++ app/(screens)/categories.tsx
@@ -1,9 +1,8 @@
 import { Text, View, StyleSheet } from "react-native";
-import s from "../styles";
 
 export default function Categories() {
   return (
-    <View style={s.defaultScreen}>
+    <View style={styles.container}>
       <Text style={styles.text}>Categories</Text>
       {/* Add tag selection for categories here */}
     </View>
@@ -11,6 +10,12 @@ export default function Categories() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#7ACDA8',
+  },
   text: {
     fontSize: 24,
     color: '#000',
--- app/(screens)/word-of-day.tsx
+++ app/(screens)/word-of-day.tsx
@@ -1,9 +1,8 @@
 import { Text, View, StyleSheet } from "react-native";
-import s from "../styles";
 
 export default function WordOfDay() {
   return (
-    <View style={s.defaultScreen}>
+    <View style={styles.container}>
       <Text style={styles.text}>Word of the Day</Text>
       {/* Add more details about the word here */}
     </View>
@@ -11,6 +10,12 @@ export default function WordOfDay() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#7ACDA8',
+  },
   text: {
     fontSize: 24,
     color: '#000',
